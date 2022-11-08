c7d470d0c030016bcb146b6ac4bc0eb530e76d4a
swemaster <64152321+swemaster@users.noreply.github.com>
2022-11-07 21:23:34 -0500

    trying to debug motion

--- src/js/motion.js
+++ src/js/motion.js
@@ -1,24 +1,14 @@
-import { animate } from "motion"
+//import { animate } from './node_modules/motion'
 
-animate(".box", { backgroundColor: "red" })
-
-const boxes = document.querySelectorAll(".box")
-
-boxes[0].classList.add("center");
-
-animate(boxes, { backgroundColor: "red" })
+const animate = require('./node_modules/motion');
 
 animate(
-    "#box",
-    { x: [0, -100, 100, 0] },
-    {
-      duration: 5,
-      offset: [0, 0.25, 0.75]
-    }
-  )
-
-  animate(
-    "#box",
-    { x: 100, rotate: 45 },
-    { duration: 0.5 }
-  )
+  "#box",
+  { rotate: 90 },
+  {
+    duration: 0.5,
+    easing: "ease-in-out",
+    repeat: 3,
+    direction: "alternate"
+  }
+)
