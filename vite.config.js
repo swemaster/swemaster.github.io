3ff796ae34d8aa58317a192b547242fe865006c5
charles <charles-francis.damedey@polymtl.ca>
2026-01-13 21:49:09 -0500

    cleaning house

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/swemaster.github.io/', // Replace 'your-repo-name' with your actual repository name
-  plugins: [react()],
-})
